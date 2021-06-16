703a9bd8d99224ba2e3793b814fef88d09aaa4ab
francesco veroli <verolifrancesco@gmail.com>
2021-06-17 00:08:04 +0200

    creacione entidad relacione 1-n con user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
- 
+ Route::get('/',[PublicController::class,'home'])->name('home');
