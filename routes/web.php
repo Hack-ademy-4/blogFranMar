7ec7adf9a5a518559fef39318ebb62888d012475
francesco veroli <verolifrancesco@gmail.com>
2021-06-17 00:30:46 +0200

    toda las rutas sin function

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,20 @@ use App\Http\Controllers\PublicController;
 */
 
  Route::get('/',[PublicController::class,'home'])->name('home');
+
+//article route
+
+ //create
+ Route::get('/articles/create',[ArticleController::class,'create'])->name('articles.create');
+ Route::post('/articles',[ArticleController::class,'store'])->name('articles.store');
+ 
+ //update
+ Route::get('/articles/{article}/edit',[ArticleController::class,'edit'])->name('articles.edit');
+ Route::put('/articles/{article}/update',[ArticleController::class,'update'])->name('articles.update');
+
+ //delete
+ Route::get('/articles/{article}',[ArticleController::class,'destroy'])->name('articles.destroy');
+ 
+ //read
+ Route::get('/articles',[ArticleController::class,'index'])->name('articles.index');
+ Route::get('/articles/{article}',[ArticleController::class,'show'])->name('articles.show');
